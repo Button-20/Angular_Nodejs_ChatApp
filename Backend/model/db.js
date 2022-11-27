4ec9282f6e3c191d9ac948c9c3521f6814852754
Jason Addy <jasonaddy51@gmail.com>
2022-11-27 14:32:34 +0000

    a

---
+++ Backend/model/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// Connect to the database
+mongoose.connect(
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB connection succeeded.");
+    } else {
+      console.log(
+        "Error in MongoDB connection : " + JSON.stringify(err, undefined, 2)
+      );
+    }
+  }
+);
+
